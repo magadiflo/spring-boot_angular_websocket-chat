68ddad839ba7599c087329a4f291e60b3004803e
Martín <magadiflo@gmail.com>
2024-03-27 12:35:46 -0500

    FrontEnd - Crea componente y servicio y agrega estilos

--- angular-websocket-chat/src/app/app.routes.ts
+++ angular-websocket-chat/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ChatComponent } from './components/chat/chat.component';
 
-export const APP_ROUTES: Routes = [];
+export const APP_ROUTES: Routes = [
+  { path: 'chat/:userId', component: ChatComponent, }
+];
